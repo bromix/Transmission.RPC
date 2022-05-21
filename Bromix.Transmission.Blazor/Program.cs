403388bd714b875e2de34f786b32ef2be8378944
bromix <matthias.bromisch@gmail.com>
2022-05-21 18:35:16 +0200

    refactor: renamed project files

---
+++ Bromix.Transmission.Blazor/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Bromix.Transmission.Blazor.Data;
+
+Bromix.Transmission.RPC.Environment.Load();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<Bromix.Transmission.RPC.Client>(serviceProvider =>
+{
+    var transmissionUrl = System.Environment.GetEnvironmentVariable("TRANSMISSION_URL") ?? throw new InvalidOperationException("TRANSMISSION_URL is missing in Environment Variables.");
+    var transmissionUserName = System.Environment.GetEnvironmentVariable("TRANSMISSION_USERNAME") ?? throw new InvalidOperationException("TRANSMISSION_USERNAME is missing in Environment Variables.");
+    var transmissionPassword = System.Environment.GetEnvironmentVariable("TRANSMISSION_PASSWORD") ?? throw new InvalidOperationException("TRANSMISSION_PASSWORD is missing in Environment Variables.");
+    Bromix.Transmission.RPC.Client client = new(
+        transmissionUrl,
+        transmissionUserName,
+        transmissionPassword
+    );
+    return client;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Bromix.Transmission.RPC/Environment.cs
@@ -0,0 +1,21 @@
+namespace Bromix.Transmission.RPC;
+
+public static class Environment
+{
+    public static void Load()
+    {
+        var pathOfTest = AppDomain.CurrentDomain.BaseDirectory;
+        var envFileName = Path.Combine(pathOfTest, ".env");
+        if (!File.Exists(envFileName)) return;
+        foreach (string line in System.IO.File.ReadLines(envFileName))
+        {
+            var index = line.IndexOf("=");
+            if (index > 0)
+            {
+                var key = line.Substring(0, index);
+                var value = line.Substring(index + 1);
+                System.Environment.SetEnvironmentVariable(key, value);
+            }
+        }
+    }
+}
