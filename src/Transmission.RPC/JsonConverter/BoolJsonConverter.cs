9a261bca69df70495fd9a4401a7794986b51a10c
bromix <matthias.bromisch@gmail.com>
2022-12-04 16:53:06 +0100

    cleanup

--- src/Transmission.RPC/JsonConverter/BoolJsonConverter.cs
+++ src/Transmission.RPC/JsonConverter/BoolJsonConverter.cs
@@ -7,20 +7,27 @@ public class BoolJsonConverter : JsonConverter<bool>
 {
     public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        if (reader.TokenType == JsonTokenType.Number) return reader.GetInt16() > 0;
-
-        if (reader.TokenType == JsonTokenType.String)
+        return reader.TokenType switch
         {
-            var value = reader.GetString()?.ToLower();
-            if (!string.IsNullOrWhiteSpace(value))
-            {
-                if (value == "true") return true;
-                if (value == "false") return false;
-            }
-        }
-
-        throw new ArgumentException($"Unsupported type {reader.TokenType}");
+            JsonTokenType.Number => NumberToBool(reader),
+            JsonTokenType.String => StringToBool(reader),
+            _ => throw new ArgumentException($"Unsupported type {reader.TokenType}")
+        };
     }
+
+    private static bool StringToBool(Utf8JsonReader reader)
+    {
+        var value = reader.GetString()?.ToLower() ?? string.Empty;
+
+        return value switch
+        {
+            "true" => true,
+            "false" => false,
+            _ => throw new ArgumentException($"Unsupported value {value}")
+        };
+    }
+
+    private static bool NumberToBool(Utf8JsonReader reader) => reader.GetInt16() > 0;
 
     public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
     {
