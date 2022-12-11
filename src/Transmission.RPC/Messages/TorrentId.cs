2a56abc1f862b7ac3cf3a3802e67c0a9589dcbcf
bromix <matthias.bromisch@gmail.com>
2022-12-11 18:15:32 +0100

    fix: implement IEquality<TorrentId>.

--- src/Transmission.RPC/Messages/TorrentId.cs
+++ src/Transmission.RPC/Messages/TorrentId.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Record to allow usage of int and string torrent ids at the same time.
 /// </summary>
-public sealed record TorrentId
+public sealed class TorrentId : IEquatable<TorrentId>
 {
     private TorrentId(int id) => Id = id;
     private TorrentId(string hashString) => Id = hashString;
@@ -11,4 +11,18 @@ public sealed record TorrentId
 
     public static implicit operator TorrentId(int id) => new(id);
     public static implicit operator TorrentId(string hashString) => new(hashString);
+
+    public static bool operator ==(TorrentId left, TorrentId right) => Equals(left, right);
+    public static bool operator !=(TorrentId left, TorrentId right) => !(left == right);
+
+    public bool Equals(TorrentId? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        if (GetType() != other.GetType()) return false;
+        return Id.Equals(other.Id);
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as TorrentId);
+    public override int GetHashCode() => Id.GetHashCode();
 }
---
+++ tests/Transmission.RPC.Tests/Messages/TorrentIdTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Transmission.RPC.Messages;
+using Xunit;
+
+namespace Transmission.RPC.Tests.Messages;
+
+public sealed class TorrentIdTests
+{
+    [Fact]
+    public void Assign_Id()
+    {
+        TorrentId torrentId = 1;
+        torrentId.Id.Should().Be(1);
+    }
+
+    [Fact]
+    public void Assign_HashString()
+    {
+        TorrentId torrentId = "3485034958345903534050";
+        torrentId.Id.Should().Be("3485034958345903534050");
+    }
+
+    [Fact]
+    public void Is_Equal()
+    {
+        TorrentId torrentId1 = 1;
+        TorrentId torrentId2 = 1;
+        torrentId1.Should().Be(torrentId1);
+        torrentId1.Should().Be(torrentId2);
+    }
+
+    [Fact]
+    public void Is_NotEqual()
+    {
+        TorrentId torrentId1 = 1;
+        TorrentId torrentId2 = 2;
+        torrentId1.Should().NotBe(torrentId2);
+    }
+}
