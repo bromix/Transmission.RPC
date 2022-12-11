1be3f11795f0099e73fcbf19b3b9e5a5bc0f61e7
bromix <matthias.bromisch@gmail.com>
2022-12-11 18:54:30 +0100

    doc: add comment.

--- src/Transmission.RPC/Messages/PortTest/PortTestRequest.cs
+++ src/Transmission.RPC/Messages/PortTest/PortTestRequest.cs
@@ -1,3 +1,6 @@
 ﻿namespace Transmission.RPC.Messages.PortTest;
 
+/// <summary>
+/// This request tests to see if your incoming peer port is accessible from the outside world.
+/// </summary>
 internal sealed record PortTestRequest;
